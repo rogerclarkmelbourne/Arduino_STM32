79668ed6673d84bfa6bff2eae0b18db2d66d5268
Roger Clark <roger@rogerclark.net>
2014-12-04 07:07:23 +1100

    Put fix for D: drive (etc) use into maple_upload.bat, that had previously only been added to serial_upload.bat

--- tools/win/maple_upload.bat
+++ tools/win/maple_upload.bat
@@ -1,10 +1,8 @@
 @echo off
-rem
-rem 
-rem
-rem parameters are: 
-
-rem Note: %~dp0 get path of this batch file, so path parameter is no longer required
-
+rem: Note %~dp0 get path of this batch file
+rem: Need to change drive if My Documents is on a drive other than C:
+set driverLetter=%~dp0
+set driverLetter=%driverLetter:~0,2%
+%driverLetter%
 cd %~dp0
 java -jar maple_loader.jar %1 %2 %3 %4 %5 %6 %7 %8 %9
