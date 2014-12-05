a327d7bd381b388b003b6386a145ae90b5d5bb98
Roger Clark <roger@rogerclark.net>
2014-12-05 21:17:45 +1100

    Removed redundant / junk files

--- STM32F1XX/variants/maple_mini/variant.h
+++ STM32F1XX/variants/maple_mini/variant.h
@@ -2,9 +2,11 @@
 #define _VARIANT_ARDUINO_STM32_
 
 // From SAM implementation #define digitalPinToBitMask(P)     ( g_APinDescription[P].ulPin )
-// Added placeholder to please the compiler
+
 
 #warning "TO DO. IMPLEMENT digitalPinToBitMask in variant.h"
+// Its likely that this function has no meaning with reference to the STM32 GPIO
+// But its required by some libraries. 
 #define digitalPinToBitMask(P)     ( 1 )
 
 #endif /* _VARIANT_ARDUINO_STM32_ */
