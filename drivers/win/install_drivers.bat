ca406ca015a4f56808bd8891fae03aae75619213
rogerclarkmelbourne <roger@ecbtechnology.com>
2015-01-23 17:26:34 +1100

    Replaced windows driver files with installer batch file and exe produced by tim schuerewegen based on a modified version of dwi-simple.

---
+++ drivers/win/install_drivers.bat
@@ -0,0 +1,9 @@
+@echo off
+
+echo Installing Maple DFU driver...
+%~dp0wdi-simple --vid 0x1EAF --pid 0x0003 --type 1 --name "Maple DFU" --dest %~dp0maple-dfu
+
+echo Installing Maple Serial driver...
+%~dp0wdi-simple --vid 0x1EAF --pid 0x0004 --type 3 --name "Maple Serial" --dest %~dp0maple-serial
+
+pause
