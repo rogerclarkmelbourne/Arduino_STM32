347cee0d4e4c540708fa9f21bca364777eebf7a6
Roger Clark <roger@rogerclark.net>
2015-04-15 09:40:54 +1000

    added comment to generic stm32f103c board.h

--- STM32F1/variants/generic_stm32f103c/board/board.h
+++ STM32F1/variants/generic_stm32f103c/board/board.h
@@ -76,7 +76,7 @@
 #define BOARD_USB_DISC_DEV        GPIOB
 #define BOARD_USB_DISC_BIT        10
 
-
+// Note this needs to match with the PIN_MAP array in board.cpp
 enum {
     PA0, PA1, PA2, PA3, PA4, PA5, PA6, PA7, PA8, PA9, PA10, PA11, PA12, PA13,PA14,PA15,
 	PB0, PB1, PB2, PB3, PB4, PB5, PB6, PB7, PB8, PB9, PB10, PB11, PB12, PB13,PB14,PB15,
