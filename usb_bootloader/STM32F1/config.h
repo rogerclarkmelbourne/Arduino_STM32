f92904f577341272dba56835e0d7e34df2262a2f
rogerclarkmelbourne <roger@ecbtechnology.com>
2015-04-25 19:15:48 +1000

    Added support for bootloader 2.0 to Maple mini, and added source and binary for maple_mini_boot20.bin to the repo

--- usb_bootloader/STM32F1/config.h
+++ usb_bootloader/STM32F1/config.h
@@ -35,17 +35,18 @@
 
 #include "common.h"
 
+/* Speed controls for strobing the LED pin */
+#define BLINK_FAST 0x50000
+#define BLINK_SLOW 0x100000
+
 /* On the Mini, LED is PB1 */
+#if defined(TARGET_MAPLE_MINI)
 #define LED_BANK         GPIOB
 #define LED              1
 #define LED_BANK_CR      GPIO_CRL(LED_BANK)
 #define LED_CR_MASK      0xFFFFFF0F
 #define LED_CR_MODE      0x00000010
 #define RCC_APB2ENR_LED  0x00000008 /* enable PB */
-
-/* Speed controls for strobing the LED pin */
-#define BLINK_FAST 0x50000
-#define BLINK_SLOW 0x100000
 
 /* On the Mini, BUT is PB8 */
 #define BUTTON_BANK      GPIOB
@@ -54,13 +55,18 @@
 #define BUT_CR_MASK      0xFFFFFFF0
 #define BUT_CR_OUTPUT_IN 0x00000004
 #define RCC_APB2ENR_BUT  0x00000008 /* enable PB */
+#else
+
+#error "No config for this target"
+
+#endif
 
 #define STARTUP_BLINKS 5
 #define BOOTLOADER_WAIT 6
 
 #define USER_CODE_RAM     ((u32)0x20000C00)
 #define RAM_END           ((u32)0x20005000)
-#define USER_CODE_FLASH   ((u32)0x08005000)
+#define USER_CODE_FLASH   ((u32)0x08002000)
 #define FLASH_END         ((u32)0x08020000)
 
 #define VEND_ID0 0xAF
--- usb_bootloader/STM32F1/main.c
+++ usb_bootloader/STM32F1/main.c
@@ -45,9 +45,7 @@ int main() {
     strobePin(LED_BANK, LED, STARTUP_BLINKS, BLINK_FAST);
 
   /* wait for host to upload program or halt bootloader */
-  bool no_user_jump =
-      (!checkUserCode(USER_CODE_FLASH) && !checkUserCode(USER_CODE_RAM)) ||
-      readPin(BUTTON_BANK,BUTTON);
+  bool no_user_jump = !checkUserCode(USER_CODE_FLASH) || readPin(BUTTON_BANK,BUTTON);
   int delay_count = 0;
 
     while ((delay_count++ < BOOTLOADER_WAIT)
@@ -60,14 +58,12 @@ int main() {
         }
     }
 
-    if (checkUserCode(USER_CODE_RAM)) {
-        jumpToUser(USER_CODE_RAM);
-    } else if (checkUserCode(USER_CODE_FLASH)) {
+	if (checkUserCode(USER_CODE_FLASH)) 
+	{
         jumpToUser(USER_CODE_FLASH);
     } else {
         // some sort of fault occurred, hard reset
         strobePin(LED_BANK, LED, 5, BLINK_FAST);
         systemHardReset();
     }
-
 }
