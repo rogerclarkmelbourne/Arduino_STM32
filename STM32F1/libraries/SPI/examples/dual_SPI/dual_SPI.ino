0c7015b84f38f997b366ea790b7585352f16e18b
Vassilis Serasidis <avrsite@yahoo.gr>
2015-06-11 14:57:52 +0300

    Added the Chip Select definitions SPI1_NSS_PIN and SPI2_NSS_PIN

--- STM32F1/libraries/SPI/examples/dual_SPI/dual_SPI.ino
+++ STM32F1/libraries/SPI/examples/dual_SPI/dual_SPI.ino
@@ -27,26 +27,29 @@
 
 #include <SPI.h>
 
+#define SPI1_NSS_PIN PA4    //SPI_1 Chip Select pin is PA4. You can change it to the STM32 pin you want.
+#define SPI2_NSS_PIN PB12   //SPI_2 Chip Select pin is PB12. You can change it to the STM32 pin you want.
+
 byte data;
 SPIClass SPI_2(2); //Create an SPI2 object.
 
 
 void setup() {
   // Initialize the SPI_1 object. SPI speed 1.125 MHz, MSB first, SPI mode = 0
-  SPI.begin(BOARD_SPI1_NSS_PIN, SPISettings(1125000, MSBFIRST, SPI_MODE0)); //Initialize the SPI_1 port.
+  SPI.begin(SPI1_NSS_PIN, SPISettings(1125000, MSBFIRST, SPI_MODE0)); //Initialize the SPI_1 port.
  
   // Initialize the SPI_2 object. SPI speed 4.5 MHz, MSB first, SPI mode = 0
-  SPI_2.begin(BOARD_SPI2_NSS_PIN, SPISettings(4500000, MSBFIRST, SPI_MODE0)); //Initialize the SPI_2 port.
+  SPI_2.begin(SPI2_NSS_PIN, SPISettings(4500000, MSBFIRST, SPI_MODE0)); //Initialize the SPI_2 port.
 }
 
 void loop() {
-  digitalWrite(BOARD_SPI1_NSS_PIN, LOW);  // assert chip select
+  digitalWrite(SPI1_NSS_PIN, LOW);  // assert chip select
   data = SPI.transfer(0x55);        //Send the HEX data 0x55 over SPI-1 port and store the received byte to the <data> variable.
-  digitalWrite(BOARD_SPI1_NSS_PIN, HIGH); // assert chip select
+  digitalWrite(SPI1_NSS_PIN, HIGH); // assert chip select
   delayMicroseconds(5);             //Delay 10 micro seconds.
  
-  digitalWrite(BOARD_SPI2_NSS_PIN, LOW);  // assert chip select
+  digitalWrite(SPI2_NSS_PIN, LOW);  // assert chip select
   data = SPI_2.transfer(0xAC);      //Send the HEX data 0xAC over SPI-2 port and store the received byte to the <data> variable.
-  digitalWrite(BOARD_SPI2_NSS_PIN, HIGH); // assert chip select
+  digitalWrite(SPI2_NSS_PIN, HIGH); // assert chip select
   delayMicroseconds(5);             //Delay 10 micro seconds.
 }
