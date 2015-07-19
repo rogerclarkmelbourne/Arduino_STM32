ce70ae4fcee762c94c6e80c3d63481f739897ff6
Roger Clark <roger@rogerclark.net>
2015-07-20 06:38:38 +1000

    Added source to maple_loader.jar

---
+++ tools/src/maple_loader/src/processing/app/Base.java
@@ -0,0 +1,53 @@
+/* -*- mode: java; c-basic-offset: 2; indent-tabs-mode: nil -*- */
+
+/*
+  Part of the Processing project - http://processing.org
+
+  Copyright (c) 2004-10 Ben Fry and Casey Reas
+  Copyright (c) 2001-04 Massachusetts Institute of Technology
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License version 2
+  as published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software Foundation,
+  Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package processing.app;
+
+
+/**
+ * The base class for the main processing application.
+ * Primary role of this class is for platform identification and
+ * general interaction with the system (launching URLs, loading
+ * files and images, etc) that comes from that.
+ */
+public class Base {
+
+  /**
+   * returns true if running on windows.
+   */
+  static public boolean isWindows() {
+    //return PApplet.platform == PConstants.WINDOWS;
+    return System.getProperty("os.name").indexOf("Windows") != -1;
+  }
+
+
+  /**
+   * true if running on linux.
+   */
+  static public boolean isLinux() {
+    //return PApplet.platform == PConstants.LINUX;
+    return System.getProperty("os.name").indexOf("Linux") != -1;
+  }
+
+
+  
+}
