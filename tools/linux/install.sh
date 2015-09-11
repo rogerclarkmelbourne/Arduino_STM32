2938d510b299ca8f3e43f30a0edad652a1f3853a
santonio <santonio@plumgrove.com.au>
2015-09-11 10:53:00 +0800

    Added rules for arch and the uucp group

--- tools/linux/install.sh
+++ tools/linux/install.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
 
+#Arch uses the uucp group, dialout is depricated
+retUUCP=false
+getent group uucp $1 >/dev/null 2>&1 && retUUCP=true
+
 if sudo [ -w /etc/udev/rules.d ]; then
     echo "Copying Maple-specific udev rules..."
-    sudo cp -v 45-maple.rules /etc/udev/rules.d/45-maple.rules
+    if $retUUCP
+    then
+        echo "Adding current user to uucp group"
+        sudo usermod -a -G uucp $USER
+        sudo cp -v 45-maple.rules.arch /etc/udev/rules.d/45-maple.rules
+    else
+        echo "Adding current user to dialout group"
+        sudo adduser $USER dialout
+        sudo cp -v 45-maple.rules /etc/udev/rules.d/45-maple.rules
+    fi
     sudo chown root:root /etc/udev/rules.d/45-maple.rules
     sudo chmod 644 /etc/udev/rules.d/45-maple.rules
     echo "Reloading udev rules"
     sudo udevadm control --reload-rules
-    echo "Adding current user to dialout group"
-    sudo adduser $USER dialout
 else
     echo "Couldn't copy to /etc/udev/rules.d/; you probably have to run this script as root? Or your distribution of Linux doesn't include udev; try running the IDE itself as root."
 fi
