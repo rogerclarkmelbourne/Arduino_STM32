3d15c75a27c36c761628c22e22fc6dcba714aa01
Martin Ayotte <martinayotte@yahoo.ca>
2016-02-29 12:12:57 -0500

    add toString() helper to IPAddress class

--- STM32F1/cores/maple/IPAddress.h
+++ STM32F1/cores/maple/IPAddress.h
@@ -21,6 +21,7 @@
 #define IPAddress_h
 
 #include <stdint.h>
+#include <WString.h>
 #include <Printable.h>
 
 // A class to make it easier to handle and pass around IP addresses
@@ -60,6 +61,7 @@ public:
     IPAddress& operator=(uint32_t address);
 
     virtual size_t printTo(Print& p) const;
+    String toString();
 
     friend class EthernetClass;
     friend class UDP;
