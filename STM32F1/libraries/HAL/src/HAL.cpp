d62cf1a35ac07145da8725d12513887f3ba008e7
Roger Clark <roger@rogerclark.net>
2016-06-21 11:33:02 +1000

    Added experimental HAL library

---
+++ STM32F1/libraries/HAL/src/HAL.cpp
@@ -0,0 +1,6 @@
+#include "HAL.h"
+
+HAL::HAL(void)
+{
+	HAL_Init();
+}
---
+++ STM32F1/libraries/HAL/src/HAL.h
@@ -0,0 +1,16 @@
+#ifndef HAL_h
+#define HAL_h
+
+#ifdef __cplusplus
+ extern "C" {
+	 #include "stm32f1xx_hal.h"
+ }
+#endif /* __cplusplus */
+
+
+class HAL  {
+public:
+   HAL(void);
+};
+
+#endif
