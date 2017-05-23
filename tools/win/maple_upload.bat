3be9e574c40950b7d2f2c9bef117d5e2aceedb53
Andrew Goh <gohandrew@yahoo.com>
2017-05-23 20:45:22 +0800

    wait few seconds (3) after dfu upload, tools/{os}/maple_upload shell script, to prevent arduino ide's serial console connecting prematurely
    discussed in this thread
    http://www.stm32duino.com/viewtopic.php?f=28&t=2107
    fixes contributed by hanyazou, danieleff, ag123

--- tools/win/maple_upload.bat
+++ tools/win/maple_upload.bat
@@ -6,3 +6,5 @@ set driverLetter=%driverLetter:~0,2%
 %driverLetter%
 cd %~dp0
 java -jar maple_loader.jar %1 %2 %3 %4 %5 %6 %7 %8 %9
+rem take a breath waiting for restarting the target device //hanyazou, danieleff
+ping -n 3 127.0.0.1 
