938f255fe31c6f3009ff11e2942c30625abdf9d8
victorpv <victor_pv@hotmail.com>
2017-08-08 15:43:55 -0500

    Update libmaple_types.h

--- STM32F4/cores/maple/libmaple/libmaple_types.h
+++ STM32F4/cores/maple/libmaple/libmaple_types.h
@@ -53,7 +53,7 @@ typedef void (*voidFuncPtr)(void);
   #define __attr_flash __attribute__((section (".USER_FLASH")))
 #endif
 #ifndef __attr_ccmram
-  #define __attr_ccmram __attribute__((section (".ccmram")))
+  #define __attr_ccmram __attribute__((section (".ccmdata")))
 #endif
 #ifndef __always_inline
   #define __always_inline inline __attribute__((always_inline))
