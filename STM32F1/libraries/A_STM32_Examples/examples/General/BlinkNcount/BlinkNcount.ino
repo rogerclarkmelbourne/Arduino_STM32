15ce8137b92f5b0cdb3c6e5ac51f02e52eb2c7a4
aka. N[]NE in gaming communities <lgtngstk@hotmail.com>
2017-11-06 08:55:32 -0500

    Update BlinkNcount.ino
    Uses `#define` for LED pin instead of 5 literals

--- STM32F1/libraries/A_STM32_Examples/examples/General/BlinkNcount/BlinkNcount.ino
+++ STM32F1/libraries/A_STM32_Examples/examples/General/BlinkNcount/BlinkNcount.ino
@@ -1,35 +1,38 @@
 /*
-  BlinkNcount for Maple Mini by m. ray burnette
+  BlinkNcount for Maple Mini by m. ray burnette, update by The Lightning Stalker to use #define
     Sketch uses 13,808 bytes (12%) of program storage space. Maximum is 108,000 bytes.
     Global variables use 2,592 bytes of dynamic memory.
   Turns on an LED on for one second, then off for one second, repeatedly.
   Counts and displays the count on the attached serial monitor
   This example code is in the public domain.
- */
+*/
+
+#define LED_PIN PB1  // Maple Mini LED is on PB1, other boards may vary
+
 int n = 0;
 
 
-void setup() {                
+void setup() {
   // initialize the digital pin as an output.
-  pinMode(33, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
   // Initialize virtual COM over USB on Maple Mini
   Serial.begin(9600);  // BAUD has no effect on USB serial: placeholder for physical UART
   // wait for serial monitor to be connected.
   while (!Serial)
   {
-    digitalWrite(33,!digitalRead(33));// Turn the LED from off to on, or on to off
+    digitalWrite(LED_PIN, !digitalRead(LED_PIN)); // Turn the LED from off to on, or on to off
     delay(100);         // fast blink
   }
   Serial.println("Blink LED & count Demo");
 }
 
 void loop() {
-  digitalWrite(33, HIGH);   // set the LED on
+  digitalWrite(LED_PIN, HIGH);   // set the LED on
   delay(500);              // wait for a second
-  digitalWrite(33, LOW);    // set the LED off
+  digitalWrite(LED_PIN, LOW);    // set the LED off
   Serial.print("Loop #: ");
   n++;
   Serial.println(n);
-   
-  delay(500);              // wait 
+
+  delay(500);              // wait
 }
