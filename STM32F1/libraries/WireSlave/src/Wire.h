56e95f2773f10b1dad148d1c5e3f93b45c5b9cd2
lacklustrlabs <lacklustrlabs@gmail.com>
2017-12-14 22:03:09 +0100

    Steals any reference to Wire.h and redirects it to Wire_slave.h. This way we don't need to alter third party libraries. Note that Wire_slave.h must be included **before** the real Wire.h for this to work.

--- STM32F1/libraries/WireSlave/src/Wire.h
+++ STM32F1/libraries/WireSlave/src/Wire.h
@@ -1,1 +1,4 @@
-#error "Something is trying to include Wire.h when Wire_slave.h is already included, they are mutually exclusive"
+#pragma once
+#include "Wire_slave.h"
+
+//#error "Something is trying to include Wire.h when Wire_slave.h is already included, they are mutually exclusive"
