f5e4f7f655cd1855de660f66a35318a564b87743
Cai Zhaotian <351920759@qq.com>
2019-01-06 20:30:00 +0800

    Add transfer(buffer,size) .
    https://www.arduino.cc/en/Reference/SPITransfer Update to new functions.

--- STM32F1/libraries/SPI/src/SPI.h
+++ STM32F1/libraries/SPI/src/SPI.h
@@ -290,6 +290,7 @@ public:
      */
     uint8 transfer(uint8 data) const;
     uint16_t transfer16(uint16_t data) const;
+    void transfer(void *buffer,size_t size) const; //Added by Cai Zhaotian
 
 	/**
      * @brief Sets up a DMA Transfer for "length" bytes.
