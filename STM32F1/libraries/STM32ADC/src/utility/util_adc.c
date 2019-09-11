87141e1f370b82feb23472ea0da9f1181b28769c
Scott Lahteine <github@thinkyhead.com>
2019-09-11 04:18:33 -0500

    Remove errant "init" of extern

--- STM32F1/libraries/STM32ADC/src/utility/util_adc.c
+++ STM32F1/libraries/STM32ADC/src/utility/util_adc.c
@@ -7,7 +7,7 @@
     Interrupt function.
     This handles Analog watchdog and ADC1 and 2.
 */
-extern volatile unsigned int adc_result = 0;
+extern volatile unsigned int adc_result;
 
 /*
     Starts a single conversion in one channel previously defined.
