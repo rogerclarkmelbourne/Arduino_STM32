b39be2d544c0ae5178a9e6783af8386b9cdba5f4
Donna Whisnant <dewhisna@users.noreply.github.com>
2019-09-12 09:27:11 -0500

    Rearchitect Wire_slave to work with i2c_slave integration in core. Improve Wire_slave functionality and complete some missing pieces, such as support for dual-slave addresses.

--- STM32F1/libraries/WireSlave/src/Wire_slave.h
+++ STM32F1/libraries/WireSlave/src/Wire_slave.h
@@ -17,14 +17,19 @@
  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
  Modified 2012 by Todd Krein (todd@krein.org) to implement repeated starts
+
+ Modified 2019 by Donna Whisnant to merge WireSlave changes with the core to
+ make slave mode work and without having conflicting data type defintions and
+ to improve the WireSlave functionality, completing some of the missing pieces.
+
  */
 
 #ifndef TwoWire_h
 #define TwoWire_h
 
 #include <inttypes.h>
-#include "Stream.h"
-#include "Arduino.h"
+#include <Stream.h>
+#include <Arduino.h>
 #include <libmaple/i2c.h>
 
 
@@ -39,108 +44,111 @@
 
 /* return codes from endTransmission() */
 typedef enum EndTranmissionCodes {
-  SUCCESS = 0, /* transmission was successful */
-  EDATA = 1, /* too much data */
-  ENACKADDR = 2, /* received nack on transmit of address */
-  ENACKTRNS = 3, /* received nack on transmit of data */
-  EOTHER = 4, /* other error */
+	SUCCESS = 0, /* transmission was successful */
+	EDATA = 1, /* too much data */
+	ENACKADDR = 2, /* received nack on transmit of address */
+	ENACKTRNS = 3, /* received nack on transmit of data */
+	EOTHER = 4, /* other error */
 } EndTranmissionCodes;
 
-class TwoWire: public Stream {
+class TwoWire: public Stream
+{
 private:
-  i2c_dev* sel_hard;
+	i2c_dev* sel_hard;
 
-  uint8_t *rxBuffer;  // lazy allocation
-  uint8_t rxBufferAllocated;
-  uint8_t rxBufferIndex;
-  uint8_t rxBufferLength;
+	uint8_t *rxBuffer;		// lazy allocation
+	size_t rxBufferAllocated;
+	size_t rxBufferIndex;
+	size_t rxBufferLength;
 
-  uint8_t *txBuffer; // lazy allocation
-  uint8_t txBufferAllocated;
-  uint8_t txBufferIndex;
-  uint8_t txBufferLength;
+	uint8_t *txBuffer;		// lazy allocation
+	size_t txBufferAllocated;
+	size_t txBufferIndex;
+	size_t txBufferLength;
 
-  bool transmitting;
+	bool transmitting;
 
-  bool master;
+	uint32_t dev_flags;
+	i2c_msg itc_msg;
+	i2c_msg itc_slave_msg;
 
-  uint8 dev_flags;
-  i2c_msg itc_msg;
-  i2c_msg itc_slave_msg;
+	// TODO : Add additional slave functions that relay the address
+	//	being accessed:
+	void (*user_onRequest)(void);
+	void (*user_onReceive)(int quantity);
 
-  void (*user_onRequest)(void);
-  void (*user_onReceive)(int quantity);
+protected:
+	bool isMaster() const { return ((dev_flags & I2C_SLAVE_MODE) == 0); }
+	void allocateRxBuffer(size_t length);
+	void allocateTxBuffer(size_t length);
 
-  void allocateRxBuffer(size_t length);
-  void allocateTxBuffer(size_t length);
+	void resetRxBuffer(void);
+	void resetTxBuffer(void);
+	uint8 process(bool stop = true);	// wrapper for i2c_master_xfer
 
-  void resetRxBuffer(void);
-  void resetTxBuffer(void);
-  uint8 process(bool stop = true); // wrapper for i2c_master_xfer
+	inline void __attribute__((always_inline)) onReceiveService(i2c_msg* msg);
+	inline void __attribute__((always_inline)) onRequestService(i2c_msg* msg);
 
-  inline void __attribute__((always_inline)) onReceiveService(i2c_msg* msg);
-  inline void __attribute__((always_inline)) onRequestService(i2c_msg* msg);
+	static void onRequestService1(i2c_msg*);
+	static void onReceiveService1(i2c_msg*);
+#if WIRE_INTERFACES_COUNT > 1
+	static void onRequestService2(i2c_msg*);
+	static void onReceiveService2(i2c_msg*);
+#endif
 
-  static void onRequestService1(i2c_msg*);
-  static void onReceiveService1(i2c_msg*);
-  #if WIRE_INTERFACES_COUNT > 1
-  static void onRequestService2(i2c_msg*);
-  static void onReceiveService2(i2c_msg*);
-  #endif
+	TwoWire(i2c_dev* i2cDevice);
+	TwoWire() = delete;
+	TwoWire(const TwoWire&) = delete;
+	TwoWire& operator=(const TwoWire&) = delete;
+	TwoWire(TwoWire&&) = delete;
+	TwoWire& operator=(TwoWire&&) = delete;
 
-  TwoWire(i2c_dev* i2cDevice);
-  TwoWire() = delete;
-  TwoWire(const TwoWire&) = delete;
-  TwoWire& operator=(const TwoWire&) = delete;
-  TwoWire(TwoWire&&) = delete;
-  TwoWire& operator=(TwoWire&&) = delete;
+	static TwoWire g_Wire;
+	#if WIRE_INTERFACES_COUNT > 1
+	static TwoWire g_Wire1;
+	#endif
 
 public:
-  // return the 'Wire' instance (using I2C1)
-  static TwoWire& getInstance();
-  #if WIRE_INTERFACES_COUNT > 1
-  // return the 'Wire1' instance (using I2C2)
-  static TwoWire& getInstance1();
-  #endif
+	// return the 'Wire' instance (using I2C1)
+	static inline TwoWire& getInstance()
+	{
+		return g_Wire;
+	}
 
-  void begin(); // master mode
-  void begin(uint8_t myAddress); //slave mode
-  void begin(int myAddress); //slave mode
-  void end();
-  void setClock(uint32_t frequencyHz);
-  void beginTransmission(uint8_t slaveAddress);
-  void beginTransmission(int slaveAddress);
-  uint8_t endTransmission(void);
-  uint8_t endTransmission(uint8_t sendStop);
+#if WIRE_INTERFACES_COUNT > 1
+	// return the 'Wire1' instance (using I2C2)
+	static inline TwoWire& getInstance1()
+	{
+		return g_Wire1;
+	}
+#endif
 
-  uint8_t requestFrom(uint8_t slaveAddress, uint8_t quantity);
-  uint8_t requestFrom(uint8_t slaveAddress, uint8_t quantity, uint8_t sendStop);
-  uint8_t requestFrom(uint8_t slaveAddress, uint8_t num_bytes, uint32_t iaddress, uint8_t isize, uint8_t sendStop);
-  uint8_t requestFrom(int slaveAddress, int quantity);
-  uint8_t requestFrom(int slaveAddress, int quantity, int sendStop);
+	void begin();	// master mode
+	void begin(uint16_t myAddress);	//slave mode
+	void begin(uint16_t myAddress1, uint16_t myAddress2);		// Dual-Address slave mode
+	void end();
+	void setClock(uint32_t frequencyHz);
+	void beginTransmission(uint16_t slaveAddress);
+	uint8_t endTransmission(bool sendStop = true);
 
-  virtual size_t write(uint8_t data);
-  virtual size_t write(const uint8_t *data, size_t quantity);
-  virtual int available(void);
-  virtual int read(void);
-  virtual int peek(void);
-  virtual void flush(void);
-  void onReceive(void (*)(int quantity));
-  void onRequest(void (*)(void));
+	uint8_t requestFrom(uint16_t slaveAddress, uint8_t quantity);
+	uint8_t requestFrom(uint16_t slaveAddress, uint8_t quantity, bool sendStop);
+	uint8_t requestFrom(uint16_t slaveAddress, uint8_t num_bytes, uint32_t iaddress, uint8_t isize, bool sendStop);
 
-  inline size_t write(unsigned long data) {
-    return write((uint8_t) data);
-  }
-  inline size_t write(long data) {
-    return write((uint8_t) data);
-  }
-  inline size_t write(unsigned int data) {
-    return write((uint8_t) data);
-  }
-  inline size_t write(int data) {
-    return write((uint8_t) data);
-  }
-  using Print::write;
+	virtual size_t write(uint8_t data) override;
+	virtual size_t write(const void *data, uint32_t quantity) override;	// Note: this is the signature of the base Print::write function
+
+	virtual int available(void) override;
+	virtual int read(void) override;
+	virtual int peek(void) override;
+	virtual void flush(void) override;
+	void onReceive(void (*)(int quantity));
+	void onRequest(void (*)(void));
+
+	using Print::write;
+
+	// Minimum I2C Buffer Length:
+	static constexpr size_t BUFFER_LENGTH  = 36;		// Use 36 instead of 32 for minimum size so we can transfer 32 bytes of data in addition to addresses for EEPROM, etc.
 };
 
 extern TwoWire& Wire;
