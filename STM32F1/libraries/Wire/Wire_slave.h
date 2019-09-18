fbdf0779e0c2e029f255e85c364762266687382a
Donna Whisnant <dewhisna@users.noreply.github.com>
2019-09-17 22:10:08 -0500

    Fix Wire.h vs Wire_slave.h header confusion. The 'Wire' library should use 'Wire.h'. 'Wire_Slave' should use 'Wire_slave.h'. This will fix issues in Arduino build systems that use filename matching to determine which libraries to include.

--- STM32F1/libraries/Wire/Wire_slave.h
+++
@@ -1,1 +0,0 @@
-#error "Something is trying to include Wire_slave.h when Wire.h is already included, they are mutually exclusive"
--- STM32F1/libraries/WireSlave/src/Wire.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "Wire_slave.h"
-
-//#error "Something is trying to include Wire.h when Wire_slave.h is already included, they are mutually exclusive"
