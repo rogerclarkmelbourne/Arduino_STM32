986ae2b5c389b807590107d339f03c7800fa8075
Donna Whisnant <dewhisna@users.noreply.github.com>
2019-09-18 00:00:12 -0500

    Update WireSlave examples to function correctly with new code. Spot-checked all of them and compiled them to make sure they build.

--- STM32F1/libraries/WireSlave/examples/i2c_libmaple_slave_reader/i2c_libmaple_slave_reader.ino
+++ STM32F1/libraries/WireSlave/examples/i2c_libmaple_slave_reader/i2c_libmaple_slave_reader.ino
@@ -5,114 +5,104 @@
 *
 *  Created on: 4 Sep 2012
 *      Author: Barry Carter <barry.carter@gmail.com>
+*
+*  Updated 2019 by Donna Whisnant to reflect new WireSlave library updates
+*
+*  Note: While this example shows how to call the core's low-level I2C
+*  functions directly, their API is less stable than the Arduino API
+*  and it's easier to get yourself into trouble with the many quirks
+*  and peculiarities of the ST I2C peripheral.  Thus, it's recommended
+*  that you instead use the WireSlave TwoWire class and the predefined
+*  Wire and Wire1 objects, which are ilustrated in the other examples.
+*
 */
 #include <Wire_slave.h>
 #include <Arduino.h>
 
-#define USE_BUFFERED_EXAMPLE 1
+i2c_msg txMsg = {};
+uint8 txBuffer[255];
 
-i2c_msg msg;
-uint8 buffer[255];
-char* const bufferAsChar = (char*)buffer; // ready type casted alias
+i2c_msg rxMsg = {};
+uint8 rxBuffer[255];
 
-volatile bool newMessage = false;
+volatile uint32_t lastMessageTime;
 
 void funcrx(i2c_msg *msg __attribute__((unused))){
-  // Received length will be in msg->length
-  newMessage = true;
+  // This function is called when data is received:
+  //  msg->addr will be our slave address being accessed
+  //  msg->data will point to our receive buffer
+  //  msg->xferred will be the length of the incoming message (number of bytes received)
+  //  msg->length is the overall size of the buffer (as it was initialized)
+
+  if (msg->xferred != 5) {
+      Serial.print("Bad data received:\r\n");
+  }
+  Serial.print("length: ");
+  Serial.println(msg->xferred);
+  for (int i=0; i<msg->xferred; i++) {
+      // Print as char
+      Serial.print((char)msg->data[i]);
+      Serial.print(' ');
+      // Print as byte
+      Serial.println(msg->data[i], 16);
+  }
+
+  lastMessageTime = millis();
 }
 
-#if USE_BUFFERED_EXAMPLE == 1
-/* We ARE using a buffer to transmit the data out.
-* Make sure you fill the buffer with the data AND you set the length correctly
-*/
 void functx(i2c_msg *msg){
-  // Cheeky. We are using the received byte of the data which is currently in
-  // byte 0 to echo it back to the master device
-  //msg->data[0] = 0x01;    // We are re-using the rx buffer here to echo the request back
-  msg->data[1] = 0x02;
-  msg->data[2] = 0x03;
-  msg->data[3] = 0x04;
-  msg->data[4] = 0x05;
+  //
+  // We will get this callback for each outgoing packet. Fill in the msg buffer
+  // with the data to transmit and set the length to the size of the message.
+  //
+  msg->data[0] = 0x31;
+  msg->data[1] = 0x32;
+  msg->data[2] = 0x33;
+  msg->data[3] = 0x34;
+  msg->data[4] = 0x35;
   msg->length = 5;
 }
-
-#else
-
-/* We are NOT using the buffered data transmission
-* We will get this callback for each outgoing packet. Make sure to call i2c_write
-* Strickly speaking, we should be sending a NACk on the last byte we want to send
-* but for this test example I am going to assume the master will NACK it when it
-* wants to stop.
-*/
-void functx(i2c_msg *msg){
-  i2c_write(I2C1, msg->data[0]);
-}
-
-#endif
 
 void setup() {
   Serial.begin(115200);
   Serial.println("libmaple I2C slave reader example");
 
-  // attach the buffer
-  msg.data = buffer;
+  // Init the message structures
+  // and attach the buffers:
+  txMsg.data = txBuffer;    // The functx callback will be called with this structure with 'data' pointing to our buffer
+  txMsg.length = 0;         // No initial transmit message length
+
+  rxMsg.data = rxBuffer;
+  rxMsg.length = sizeof(rxBuffer);      // The rxMsg buffer should reflect the size of the buffer available to receive
 
   /* Init slave mode. Enables master too
+  *
   * We are going to configure the slave device to
   *  - enable fast I2C (400khz)
   *  - dual addresses (can have 2 addresses per module)
   * general call (accepts data writes to 0x00 on a broadcast basis)
-  *
-  * If the buffered example is enabled, then we also enable the
-  * buffer for rx and tx.
-  * Note you can independently enable/disable RX and TX buffers to
-  * allow a buffered read and direct writes. Useful if you don't know how
-  * much the master will read.
+  * 
   */
-  #if USE_BUFFERED_EXAMPLE == 1
-  i2c_slave_enable(I2C1, I2C_FAST_MODE | I2C_SLAVE_DUAL_ADDRESS | I2C_SLAVE_GENERAL_CALL | I2C_SLAVE_USE_RX_BUFFER | I2C_SLAVE_USE_TX_BUFFER);
-  #else
   i2c_slave_enable(I2C1, I2C_FAST_MODE | I2C_SLAVE_DUAL_ADDRESS | I2C_SLAVE_GENERAL_CALL);
-  #endif
 
   // attach  receive handler
-  i2c_slave_attach_recv_handler(I2C1, &msg, funcrx);
+  i2c_slave_attach_recv_handler(I2C1, &rxMsg, funcrx);
   // attach transmit handler
-  i2c_slave_attach_transmit_handler(I2C1, &msg, functx);
+  i2c_slave_attach_transmit_handler(I2C1, &txMsg, functx);
 
-  // set addresss to 4
+  // set address #1 to 4
   i2c_slave_set_own_address(I2C1, 4);
+
+  // set address #2 to 5
+  i2c_slave_set_own_address2(I2C1, 5);
+
+  // Start clock
+  lastMessageTime = millis();
 }
 
 void loop() {
-  static uint32_t lastMessage = millis();
-
-  // This is potentially dangerous.
-  // We're reading from the live buffer, the content can change
-  // in the middle of Serial.println
-
-  if (newMessage && strlen(bufferAsChar)==6) {
-    for (int i=0; i<5; i++) {
-      // Print as char
-      Serial.print(bufferAsChar[i]);
-    }
-    // Print as byte
-    Serial.println(buffer[5]);
-    lastMessage = millis();
-    newMessage = false;
-  } else {
-    if(newMessage && strlen(bufferAsChar)!=6) {
-      // this also happends on the line "x is 0"
-      Serial.print("Bad data received:");
-      Serial.println(bufferAsChar);
-      Serial.print("strlen:");
-      Serial.println(strlen(bufferAsChar));
-      newMessage = false;
-    } else
-    if(millis() - lastMessage > 3000){
-      Serial.println("Nothing received in 3 seconds.");
-      lastMessage = millis();
-    }
+  if((millis() - lastMessageTime) > 3000){
+    Serial.println("Nothing received in 3 seconds.");
+    lastMessageTime = millis();
   }
 }
