e2f1740fc4aea0bdef183072b2cdec1614e78f06
michox <33625058+michox@users.noreply.github.com>
2019-10-03 10:08:28 +0200

    added calibration and pixel calculation

--- STM32F1/libraries/Serasidis_XPT2046_touch/src/XPT2046_touch.cpp
+++ STM32F1/libraries/Serasidis_XPT2046_touch/src/XPT2046_touch.cpp
@@ -79,6 +79,90 @@ boolean XPT2046_touch::read_XY(uint16_t *xy) {
     return p.z > 0;
 }
 
+void XPT2046_Touchscreen::update()
+{
+	int16_t data[6];
+
+	if (!isrWake) return;
+	uint32_t now = millis();
+	if (now - msraw < MSEC_THRESHOLD) return;
+	
+	SPI.beginTransaction(SPI_SETTING);
+	digitalWrite(csPin, LOW);
+	SPI.transfer(0xB1 /* Z1 */);      //ask for Z1 value
+	int16_t z1 = SPI.transfer16(0xC1 /* Z2 */) >> 3;      //store Z1 value, ask for Z2
+	int z = z1 + 4095;
+	int16_t z2 = SPI.transfer16(0x91 /* X */) >> 3;       //store Z2 value, ask for X
+	z -= z2;
+	if (z >= Z_THRESHOLD) {
+		data[0] = SPI.transfer16(0xD1 /* Y */) >> 3;
+		data[1] = SPI.transfer16(0x91 /* X */) >> 3; // make 3 x-y measurements
+		data[2] = SPI.transfer16(0xD1 /* Y */) >> 3;
+		data[3] = SPI.transfer16(0x91 /* X */) >> 3;
+	}
+	else data[0] = data[1] = data[2] = data[3] = 0;	// Compiler warns these values may be used unset on early exit.
+	data[4] = SPI.transfer16(0xD0 /* Y */) >> 3;	// Last Y touch power down
+	data[5] = SPI.transfer16(0) >> 3;
+	digitalWrite(csPin, HIGH);
+	SPI.endTransaction();
+	if (z < 0) z = 0;
+	if (z < Z_THRESHOLD) { //	if ( !touched ) {
+		zraw = 0;
+		if (z < Z_THRESHOLD_INT) { //	if ( !touched ) {
+			if (255 != tirqPin) isrWake = false;
+		}
+		return;
+	}
+	zraw = z;
+	
+	// Average pair with least distance between each measured x then y
+	int16_t x = besttwoavg( data[0], data[2], data[4] );
+	int16_t y = besttwoavg( data[1], data[3], data[5] );
+	
+	if (z >= Z_THRESHOLD) {
+		msraw = now;	// good read completed, set wait
+          if (alpha_x) {           //if calibration parameters have been defined,
+                                   //calculate pixel coordinates
+               xraw = alpha_x * x + beta_x * y + delta_x;
+               yraw = alpha_y * x + beta_y * y + delta_y;
+               if (displ_rot) {    //and if the screen has been rotated
+                    uint16_t rot_x;
+                    uint16_t rot_y;
+                                   //calculate pixel positions for rotated screen
+                    if (displ_rot == 1) {
+                         rot_x = yraw;
+                         rot_y = displ_height - xraw;
+                    }
+                    else if (displ_rot == 2) {
+                         rot_x = displ_width - xraw;
+                         rot_y = displ_height - yraw;
+                    }
+                    else if (displ_rot == 3) {
+                         rot_x = displ_width - yraw;
+                         rot_y = xraw;
+                    }
+                    xraw = rot_x;
+                    yraw = rot_y;
+               }
+          }
+          else {
+     		xraw = x;
+     		yraw = y;
+          }
+	}
+}
+void XPT2046_touch::calibrate(float ax, float bx, int16_t dx, float ay, float by, int16_t dy, uint16_t dwidth, uint16_t dheight, uint8_t drot) { 
+     alpha_x = ax;
+     beta_x = bx;
+     delta_x = dx;
+     alpha_y = ay;
+     beta_y = by;
+     delta_y = dy;
+     displ_width = dwidth;
+     displ_height = dheight;
+     displ_rot = drot;
+}
+
 ////////////////// Buttons ////////////////
 void XPT2046_touch::setButtonsNumber(byte columnButtons, byte rowButtons ){
     _rowButtons = rowButtons;
