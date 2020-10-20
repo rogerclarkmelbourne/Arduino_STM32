7a7cff82048638b1c93ea10a313f4f6d838c4e48
stevstrong <stevestrong@online.de>
2020-10-21 00:18:27 +0200

    Update boards.h
    - corrected path delimiter

--- STM32F1/cores/maple/boards.h
+++ STM32F1/cores/maple/boards.h
@@ -125,7 +125,7 @@ bool boardUsesPin(uint8 pin);
 #define BOARD_BUTTON_PRESSED_LEVEL      HIGH
 #endif
 
-#include "series\usart.h"
+#include "series/usart.h"
 
 /**
  * @brief Does the board break out a USART/UART's RX and TX pins?
