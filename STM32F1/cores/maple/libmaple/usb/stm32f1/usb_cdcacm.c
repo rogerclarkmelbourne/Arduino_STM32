bfbf05fae01aba904924c0519ca6745c18332815
Arjan Mels <43108771+arjanmels@users.noreply.github.com>
2021-02-06 16:02:41 +0100

    Update usb_cdcacm.c
    Fixed error in peek_ex

--- STM32F1/cores/maple/libmaple/usb/stm32f1/usb_cdcacm.c
+++ STM32F1/cores/maple/libmaple/usb/stm32f1/usb_cdcacm.c
@@ -512,7 +512,7 @@ uint32 usb_cdcacm_peek_ex(uint8* buf, uint32 offset, uint32 len)
 {
     uint32 i;
     uint32 tail = (rx_tail + offset) & CDC_SERIAL_RX_BUFFER_SIZE_MASK ;
-	uint32 rx_unread = (rx_head-tail) & CDC_SERIAL_RX_BUFFER_SIZE_MASK;
+	uint32 rx_unread = (rx_head-rx_tail) & CDC_SERIAL_RX_BUFFER_SIZE_MASK;
 
     if (len + offset > rx_unread) {
         len = rx_unread - offset;
